413c919de6fdfbc4de23feafcfe864b45ac53cab
Junsok Huhh <anarinsk@gmail.com>
2019-04-21 17:20:53 +0900

    modifying code

--- process_report.R
+++ process_report.R
@@ -14,4 +14,65 @@ mueller_report <- tibble(
   ungroup() %>% 
   select(page, line, text)
 
-write_csv(mueller_report, "mueller_report.csv")
+write_csv(mueller_report, "mueller_report_2.csv")
+
+librarian::shelf(ggpage)
+
+mueller_report_csv <- "mueller_report_2.csv"
+mueller_report <- read_csv(mueller_report_csv)
+mueller_report
+
+mueller_pages <- function(highlight){
+  mueller_report %>% 
+  # pad pages with fewer lines than expected
+  complete(
+    page, 
+    line = 1:max(mueller_report$line),
+    fill = list(text = "")
+  ) %>% 
+  # Pre-process for {ggpage}
+  ggpage_build(
+    ncol = 30, 
+    bycol = FALSE, 
+    page.col = "page", 
+    wtl = FALSE, 
+    x_space_pages = 10,
+    y_space_pages = 100
+  ) %>% 
+  mutate(
+    color = case_when(
+      str_detect(word, highlight) ~ highlight,
+      TRUE ~ "normal"
+    )
+  ) 
+}
+mueller_pages0 <- mueller_pages(hl0)
+
+plot_pages <- function(data){ 
+  
+  setdiff(unique(data$color), 'normal') -> my_title 
+  
+  ggpage_plot(data) +
+  aes(fill = color) +
+  scale_fill_manual(
+    values = c("#d0d0d0", "red"), 
+    breaks = "#d0d0d0"
+  ) +
+  labs(fill = NULL, caption = "@grrrck", title = my_title) +
+  theme(legend.position = "bottom")
+}
+plot_pages(mueller_pages0)
+
+gen_nestdf <- function(kwordv){
+ mueller_pages(kwordv) %>% 
+    mutate(
+      kword = kwordv
+    ) %>% 
+  nest(-kword) %>% 
+  mutate(
+    plots = map(.$data, plot_pages)
+  )
+}
+
+gen_nestdf('russia') -> vdf 
+c('trump|president', 'russia') %>% map_df(gen_nestdf) -> vdf
